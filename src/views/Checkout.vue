12d61d46afb78898fc1c2768f6127499fba617bd
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-11-22 07:28:48 +0800

    style: add Checkout template

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,3 +1,43 @@
 <template>
-  <h1>Checkout</h1>
+  <section class="main__content">
+    <form action="" class="normalForm">
+            <p class="normalForm__title">付款資訊</p>
+            <div class="normalForm__content">
+              <div class="normalForm__group">
+                <label for="" class="normalForm__label">持卡人姓名</label>
+                <input
+                  type="text"
+                  placeholder="John Doe"
+                  class="normalForm__input"
+                />
+              </div>
+              <div class="normalForm__group">
+                <label for="" class="normalForm__label">卡號</label>
+                <input
+                  type="text"
+                  placeholder="1111 2222 3333 4444"
+                  class="normalForm__input"
+                />
+              </div>
+              <div class="normalForm__combine--1-1">
+                <div class="normalForm__group">
+                  <label for="" class="normalForm__label">有效期限</label>
+                  <input
+                    type="text"
+                    placeholder="MM/YY"
+                    class="normalForm__input"
+                  />
+                </div>
+                <div class="normalForm__group">
+                  <label for="" class="normalForm__label">CVC/CCV</label>
+                  <input
+                    type="text"
+                    placeholder="123"
+                    class="normalForm__input"
+                  />
+                </div>
+              </div>
+            </div>
+          </form>
+  </section>
 </template>
