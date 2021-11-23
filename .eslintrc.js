17a00ecaf08def55030cadd1ab8d4dc68e16f99e
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-11-24 04:23:39 +0800

    chore: set eslint rules for vue3 setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,11 @@ module.exports = {
     parser: 'babel-eslint',
   },
   rules: {
+    "no-unused-vars":"off",
+    "no-undef":"off",
+    "no-param-reassign": ["error", { "props": false }],
+    "no-return-assign": "off",
+    "import/first": "off",
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
