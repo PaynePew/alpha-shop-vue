d03c631c915391f4b264f347972c1821c76efeb4
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-11-24 04:24:56 +0800

    style: delete vue default template

--- src/views/About.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
-
-<style lang="sass" scoped>
-@import "../assets/scss/components/_address.scss"
-</style>
