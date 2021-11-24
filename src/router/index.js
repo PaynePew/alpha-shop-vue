ed012a9707be0c3a4f6b771ff80722e12ded16a4
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-11-25 05:50:35 +0800

    style: whitespace delete

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 const routes = [
   {
     path: '/',
-    redirect:{name:'Address'}
+    redirect: { name: 'Address' },
   },
   {
     path: '/address',
