5dae90fa34f39f57be7617d80f7ae7a9b209e65b
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-11-25 05:52:03 +0800

    refactor: reset App.vue to Vuex style

--- src/App.vue
+++ src/App.vue
@@ -5,103 +5,17 @@
       <Stepper />
       <router-view />
       <Cart />
-      <router-view
-      @shipping="handleShipping"
-      @after-submit="handleSubmit"
-      />
-      <Cart
-        :cart-store="cartStore"
-        :total-price="totalPrice"
-        :shipping-fee="shippingFee"
-        @add-amount="handleAddAmount"
-        @down-amount="handleDownAmount"
-      />
-      <PageButton />
     </section>
   </section>
   <Footer />
 </template>
 
-<script>
-const dummyData = [
-  {
-    id: 1,
-    product: '破壞補丁修身牛仔褲',
-    price: 3999,
-    picture: 'item-1@2x.png',
-    amount: 1,
-  },
-  {
-    id: 2,
-    product: '刷色直筒牛仔褲',
-    price: 1299,
-    picture: 'item-2@2x.png',
-    amount: 1,
-  },
-];
-</script>
-
 <script setup>
-import {
-  watch, ref, reactive, onMounted, toRefs,
-} from 'vue';
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
 import Stepper from './components/Stepper.vue';
 import Cart from './components/Cart.vue';
-import PageButton from './components/PageButton.vue';
 
-export default {
-  components: {
-    Header,
-    Footer,
-    Stepper,
-    Cart,
-    PageButton,
-// Reactive Data
-const cartStore = ref([]);
-const priceState = reactive({
-  shippingFee: 0,
-  totalPrice: 0,
-});
-const { shippingFee, totalPrice } = toRefs(priceState);
-
-// Function Section
-function handleAddAmount(itemId) {
-  cartStore.value.forEach((_el) => (_el.id === itemId ? (_el.amount += 1) : _el));
-}
-function handleDownAmount(itemId) {
-  cartStore.value.forEach((_el) => {
-    if (_el.id === itemId && _el.amount > 0) {
-      _el.amount -= 1;
-    }
-  });
-}
-function handleTotalPrice() {
-  return (
-    cartStore.value.reduce((a, b) => a.amount * a.price + b.amount * b.price)
-    + shippingFee.value
-  );
-}
-function handleShipping(fee) {
-  shippingFee.value = fee;
-}
-// LifeCycle Section
-onMounted(async () => {
-  // 模擬fetch data
-  const response = await dummyData;
-  cartStore.value = await response;
-});
-
-// Watch List
-watch(
-  [cartStore, shippingFee],
-  () => {
-    totalPrice.value = handleTotalPrice();
-  },
-};
-  { deep: true },
-);
 </script>
 
 <style lang="scss" >
