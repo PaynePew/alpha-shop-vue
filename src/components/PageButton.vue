cf161ee7ec60ef1185bcd0588ee85acd401d0777
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-11-25 05:56:21 +0800

    refactor: change Option API to Composition API and dispatch UPDATE_INFO

--- src/components/PageButton.vue
+++ src/components/PageButton.vue
@@ -2,7 +2,10 @@
   <section class="main__subNav">
     <div v-if="$route.name === 'Address'">
       <div class="btn__group--scale">
-        <button @click="nextPage()" class="btn btn--scale btn--next">
+        <button
+          @click="nextPage()"
+          class="btn btn--scale btn--next"
+        >
           下一步 &#8594;
         </button>
       </div>
@@ -18,21 +21,32 @@
     <div v-if="$route.name === 'Checkout'">
       <div class="btn__group--between">
         <button @click="prevPage()" class="btn btn--pre">&#8592; 上一步</button>
-        <button @click="nextPage()" class="btn btn--next">確認下單</button>
+        <button @click="updateInfo" class="btn btn--next">確認下單</button>
       </div>
     </div>
   </section>
 </template>
 
-<script>
-export default {
-  methods: {
-    prevPage() {
-      this.$router.push({ name: this.$router.currentRoute.value.meta.prev });
-    },
-    nextPage() {
-      this.$router.push({ name: this.$router.currentRoute.value.meta.next });
-    },
-  },
-};
+<script setup>
+import { useStore } from 'vuex';
+import { useRouter } from 'vue-router';
+import { UPDATE_INFO } from '../store/mutation-types';
+
+const props = defineProps({
+  checkInfo: Object,
+});
+
+const store = useStore();
+const router = useRouter();
+
+function updateInfo() { store.dispatch(UPDATE_INFO, props.checkInfo); }
+
+function nextPage() {
+  router.push({ name: router.currentRoute.value.meta.next });
+  updateInfo();
+}
+function prevPage() {
+  router.push({ name: router.currentRoute.value.meta.prev });
+}
+
 </script>
