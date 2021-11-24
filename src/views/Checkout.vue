bee5ea427813784a682ea1507f2cfbf4efd7e74f
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-11-25 05:58:58 +0800

    refactor: import store checkInfo to handle form and pass props to button

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -9,6 +9,7 @@
                   type="text"
                   placeholder="John Doe"
                   class="normalForm__input"
+                  v-model="checkInfo.cardHolder"
                 />
               </div>
               <div class="normalForm__group">
@@ -17,6 +18,7 @@
                   type="text"
                   placeholder="1111 2222 3333 4444"
                   class="normalForm__input"
+                  v-model="checkInfo.cardNumber"
                 />
               </div>
               <div class="normalForm__combine--1-1">
@@ -26,6 +28,7 @@
                     type="text"
                     placeholder="MM/YY"
                     class="normalForm__input"
+                    v-model="checkInfo.expired"
                   />
                 </div>
                 <div class="normalForm__group">
@@ -34,10 +37,22 @@
                     type="text"
                     placeholder="123"
                     class="normalForm__input"
+                    v-model="checkInfo.valid"
                   />
                 </div>
               </div>
             </div>
           </form>
   </section>
+  <PageButton :check-info="checkInfo"/>
 </template>
+
+<script setup>
+import { useStore } from 'vuex';
+import PageButton from '../components/PageButton.vue';
+
+const store = useStore();
+// 初始info再宣告checkInfo不與Vuex響應，update時再做更新
+const initCheckInfo = store.state.checkInfo.info;
+const checkInfo = { ...initCheckInfo };
+</script>
