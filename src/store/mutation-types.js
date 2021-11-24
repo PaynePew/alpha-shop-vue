8b86b39032720e1e6e3f1a7c7a392fe7ef57ce92
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-11-25 05:47:03 +0800

    feat: add mutation-types

---
+++ src/store/mutation-types.js
@@ -0,0 +1,7 @@
+export const ADD_AMOUNT = 'ADD_AMOUNT';
+export const DOWN_AMOUNT = 'DOWN_AMOUNT';
+export const GET_PRODUCT = 'GET_PRODUCT';
+export const UPDATE_PRICE = 'UPDATE_PRICE';
+export const UPDATE_SHIPPING = 'UPDATE_SHIPPING';
+export const UPDATE_INFO = 'UPDATE_INFO';
+export const GET_INFO = 'GET_INFO';
