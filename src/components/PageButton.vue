1284ad36f2e1cb167db311bb08b1307ce53f640e
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-11-25 17:55:05 +0800

    fix: fix checkout view submit problem

--- src/components/PageButton.vue
+++ src/components/PageButton.vue
@@ -2,10 +2,7 @@
   <section class="main__subNav">
     <div v-if="$route.name === 'Address'">
       <div class="btn__group--scale">
-        <button
-          @click="nextPage()"
-          class="btn btn--scale btn--next"
-        >
+        <button @click="nextPage()" class="btn btn--scale btn--next">
           下一步 &#8594;
         </button>
       </div>
@@ -21,16 +18,27 @@
     <div v-if="$route.name === 'Checkout'">
       <div class="btn__group--between">
         <button @click="prevPage()" class="btn btn--pre">&#8592; 上一步</button>
-        <button @click="updateInfo" class="btn btn--next">確認下單</button>
+        <button
+          @click="
+            updateInfo();
+            showModal = true;
+          "
+          class="btn btn--next"
+        >
+          確認下單
+        </button>
       </div>
     </div>
   </section>
+  <CheckModal v-if="showModal" @close="showModal = false" />
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { useStore } from 'vuex';
 import { useRouter } from 'vue-router';
 import { UPDATE_INFO } from '../store/mutation-types';
+import CheckModal from './CheckModal.vue';
 
 const props = defineProps({
   checkInfo: Object,
@@ -38,8 +46,11 @@ const props = defineProps({
 
 const store = useStore();
 const router = useRouter();
+const showModal = ref(false);
 
-function updateInfo() { store.dispatch(UPDATE_INFO, props.checkInfo); }
+function updateInfo() {
+  store.dispatch(UPDATE_INFO, props.checkInfo);
+}
 
 function nextPage() {
   router.push({ name: router.currentRoute.value.meta.next });
@@ -48,5 +59,4 @@ function nextPage() {
 function prevPage() {
   router.push({ name: router.currentRoute.value.meta.prev });
 }
-
 </script>
