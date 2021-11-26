4ed6e8198c6129e735e51ddad77c3c1a6402cc4e
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-11-26 13:27:43 +0800

    document: deploy.sh typo fix

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ cd dist
 git init 
 git add -A
 git commit -m 'deploy'
-# 部署到 https://github.com/chou0728/eric-project.git 分支為 gh-pages
+# 部署到 https://github.com/PaynePew/alpha-shop-vue.git 分支為 gh-pages
 git push -f https://github.com/PaynePew/alpha-shop-vue.git master:gh-pages
 #將dist資料夾中的內容推送至遠端gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
 cd -
